ea4387a61ec1ecb38f8a3cc58bd8db0a024d3224
sofrone7 <79331990+sofrone7@users.noreply.github.com>
2021-02-19 19:38:16 +0100

    Add files via upload

---
+++ Practica1/iroom/datos.js
@@ -0,0 +1,11 @@
+function functionDatos() {
+	alert('Esta funcion no esta implementada')
+}
+
+/*var source = new EventSource("demo_sse.php")
+
+source.onmessage = function(e) {
+	var newElement = document.createElement("li");
+	newElement.innerHTML = "message: " + e.data;
+	eventList.appendChild(newElement);
+}*/
